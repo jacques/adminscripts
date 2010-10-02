9a1cc15bd77d0b8f9c3b9364f9e28c1ac5bce281
Jacques Marneweck <jacques@powertrip.co.za>
2010-10-02 20:33:05 +0200

    Disable git diffs and backing up the configs each time sed runs.

--- installclamd.sh
+++ installclamd.sh
@@ -3,9 +3,6 @@
 pkgin up
 pkgin in clamav
 
-gsed -i'.bak' '/^Example/s/Example/#Example/' freshclam.conf clamd.conf
-git diff freshclam.conf clamd.conf
-gsed -i'.bak2' '/^#DatabaseMirror/s/XY/us/' freshclam.conf
-git diff freshclam.conf clamd.conf
-gsed -i'.bak3' '/^#DatabaseMirror/s/#//' freshclam.conf
-git diff freshclam.conf clamd.conf
+gsed -i'' '/^Example/s/Example/#Example/' freshclam.conf clamd.conf
+gsed -i'' '/^#DatabaseMirror/s/XY/us/' freshclam.conf
+gsed -i'' '/^#DatabaseMirror/s/#//' freshclam.conf
