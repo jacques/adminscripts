3a3d0704acd62a255f7d81179b05ab9903a645ed
Jacques Marneweck <jacques@powertrip.co.za>
2010-10-30 23:40:39 +0200

    Always assume we want to install instead of having to type y enter

--- installclamd.sh
+++ installclamd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install clamav
 pkgin up
-pkgin in clamav
+pkgin -y in clamav
 
 gsed -i'' '/^Example/s/Example/#Example/' freshclam.conf clamd.conf
 gsed -i'' '/^#DatabaseMirror/s/XY/us/' freshclam.conf
