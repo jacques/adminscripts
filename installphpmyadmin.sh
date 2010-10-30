0be9a6434d4e39b067e50aede6c37c7bd985c0d2
Jacques Marneweck <jacques@powertrip.co.za>
2010-10-30 23:36:24 +0200

    Bump phpMyAdmin version and symlink it afterwards.

--- installphpmyadmin.sh
+++ installphpmyadmin.sh
@@ -2,7 +2,7 @@
 # Downloads and installs phpMyAdmin
 #
 # Copyright 2005-2010 Jacques Marneweck <jacques@powertrip.co.za>
-PHPMYADMIN_VERSION=3.3.7
+PHPMYADMIN_VERSION=3.3.8
 
 wget http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/${PHPMYADMIN_VERSION}/phpMyAdmin-${PHPMYADMIN_VERSION}-all-languages.tar.bz2?use_mirror=softlayer
 
@@ -12,3 +12,6 @@ cp config.sample.inc.php config.inc.php
 
 HASH=$(dd if=/dev/random bs=32 count=1 2>/dev/null | openssl base64)
 gsed -i"" -e "s/blowfish_secret'] = ''/blowfish_secret'] = '${HASH}'/g" config.inc.php
+
+cd ..
+ln -s phpMyAdmin-${PHPMYADMIN_VERSION}-all-languages phpMyAdmin
